29e26a2b90e0551675938bfc322cd97756f11ff8
idelc <idel012@ucr.edu>
2019-03-01 09:44:54 -0800

    Charmander made

--- Charmander.cpp
+++ Charmander.cpp
@@ -1,5 +1,11 @@
 #include "Charmander.h"
 
+Charmander::Charmander(){
+      health = 39; 
+      sp_attack = 60;
+      type = Fire;
+}
+
 void Charmander::attack(Pokemon *poke){
    /*
     The attack functions does 3/5 times the sp_attack of damage to the 
@@ -7,6 +13,19 @@ void Charmander::attack(Pokemon *poke){
     critical hit ratio is 1/8
     if there is a critical, output it
    */
+  int damage = 0;
+  if((rand()%8) == 4){
+     cout << "Critical hit!" << endl;
+     damage += (2 * ((3/5) * sp_attack));
+  }
+  else{
+     damage += ((3/5) * sp_attack);
+  }
+  if(type == Grass) {
+     damage += (2 * ((3/5) * sp_attack));
+  }
+
+  health -= damage;
 }
 
 
