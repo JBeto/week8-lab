3a4f5809851a6afa773dbaa6117dc2709bd1c0d7
Karen Gonzalez <32967295+kook97@users.noreply.github.com>
2019-03-04 09:42:26 -0800

    Squirtle files

--- Squirtle.cpp
+++ Squirtle.cpp
@@ -1,5 +1,11 @@
 #include "Squirtle.h"
 
+void Squirtle::Squirtle(){
+   hp = 44;
+   sp_attack = 50;
+   type = Water;
+}
+
 void Squirtle:: attack(Pokemon *poke){
    /*
     The attack functions does 4/5 times the sp_attack of damage to the 
@@ -7,6 +13,19 @@ void Squirtle:: attack(Pokemon *poke){
      Critical hit ratio is 1/16
      if there is a critical, output it
    */
+  int damage = 0;
+  damage +=((4/5)*sp_attack);
+
+  if(poke->type == Fire){ //super effective
+      damage *= 2;
+  }
+
+  if((rand() % 16) == 8){
+     damage *= 2;
+     cout << "Critical Hit!!" << endl;
+  }
+
+  poke->hp -= damage;
 }
 
 
